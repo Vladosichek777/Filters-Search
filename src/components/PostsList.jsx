3f2eacc084ee8fe697e56c7c1ab9d306798672c1
Vladyslav <ogurez111@gmail.com>
2025-07-15 12:35:34 +0200

    refactor: change react-window for react-virtuoso

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,38 +1,35 @@
 import Stack from "@mui/material/Stack";
-// import Post from "./Post";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { FixedSizeList as List } from "react-window";
+import { Virtuoso } from "react-virtuoso";
 
 export default function PostsList({ listPosts, deletePost }) {
-  function Post({ index, style }) {
-    return (
-      <Box style={style}>
-        <Paper sx={{ padding: "10px 30px" }} variant="elevation" elevation={5}>
-          <Stack direction="row" justifyContent="space-between" alignItems="center">
-            <Box>
-              <Typography sx={{ textDecoration: "underline" }} variant="subtitle1" gutterBottom>
-                {listPosts[index].title}
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                {listPosts[index].body}
-              </Typography>
-            </Box>
-            <IconButton onClick={() => deletePost(listPosts[index].id)} aria-label="delete">
-              <DeleteIcon color="error" />
-            </IconButton>
-          </Stack>
-        </Paper>
-      </Box>
-    );
-  }
-
   return (
-    <List sx={{ marginTop: "8vh" }} height={400} itemCount={listPosts.length} itemSize={150} width={"70%"}>
-      {Post}
-    </List>
+    <Virtuoso
+      style={{ height: "80vh", width: "70%" }}
+      data={listPosts}
+      itemContent={(_, post) => (
+        <Box>
+          <Paper sx={{ padding: "10px 30px", marginBottom: "20px" }} variant="elevation" elevation={5}>
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+              <Box>
+                <Typography sx={{ textDecoration: "underline" }} variant="subtitle1" gutterBottom>
+                  {post.title}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {post.body}
+                </Typography>
+              </Box>
+              <IconButton onClick={() => deletePost(post.id)} aria-label="delete">
+                <DeleteIcon color="error" />
+              </IconButton>
+            </Stack>
+          </Paper>
+        </Box>
+      )}
+    />
   );
 }
